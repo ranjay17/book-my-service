b53319a9d3083a61d52eca48f9b2aca524410af0
ranjay17 <sranjay15@gmail.com>
2026-06-25 13:01:10 +0530

    Fix protected route crash and auth handling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import Header from "./Components/Header";
 import VendorHeader from "./Components/VendorHeader";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { addUser, removeUser } from "./redux/userSlice";
 import { jwtDecode } from "jwt-decode";
@@ -9,22 +9,23 @@ import { jwtDecode } from "jwt-decode";
 const App = () => {
   const userStore = useSelector((store) => store.user.user);
   const dispatch = useDispatch();
-  const navigate = useNavigate();
 
   useEffect(() => {
     const token = localStorage.getItem("token");
     const user = localStorage.getItem("user");
+
     if (!token) {
       dispatch(removeUser());
-      navigate("/login");
       return;
     }
 
     try {
       const decoded = jwtDecode(token);
+
       if (decoded.exp * 1000 < Date.now()) {
         dispatch(removeUser());
-        navigate("/login");
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
         return;
       }
 
@@ -33,7 +34,6 @@ const App = () => {
       }
     } catch (err) {
       dispatch(removeUser());
-      navigate("/login");
     }
   }, []);
 
--- client/src/Components/ProtectedUserRoute.jsx
+++ client/src/Components/ProtectedUserRoute.jsx
@@ -4,12 +4,17 @@ import { Navigate } from "react-router-dom";
 
 const ProtectedUserRoute = ({ children }) => {
   const user = useSelector((store) => store.user.user);
+  const token = localStorage.getItem("token");
 
-  if (!user) {
+  if (!user && !token) {
     return <Navigate to="/login" replace />;
   }
 
-  if (user.role !== "user") {
+  if (!user && token) {
+    return <div>Loading...</div>;
+  }
+
+  if (user?.role !== "user") {
     return <Navigate to="/" replace />;
   }
 
