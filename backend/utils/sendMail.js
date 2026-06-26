920fbbec05151778add3b79ac4e49c9b7dbd192f
ranjay17 <sranjay15@gmail.com>
2026-06-26 22:52:06 +0530

    Add SMTP debug logs for production

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -22,18 +22,28 @@ const transporter = nodemailer.createTransport({
   socketTimeout: 30000,
 });
 
-const sendMail = async(to, subject, text) =>{
-    try {
-        await transporter.sendMail({
-          from: process.env.EMAIL,
-          to,
-          subject,
-          text,
-        });
-        console.log("Email sent successfully");
-    } catch (error) {
-        console.log(error);
-    }
-}
+const sendMail = async (to, subject, text) => {
+  try {
+    console.log("EMAIL:", process.env.EMAIL);
+    console.log(
+      "EMAIL_PASS:",
+      process.env.EMAIL_PASS ? "PASS FOUND" : "PASS MISSING",
+    );
+
+    await transporter.verify();
+    console.log("SMTP Verified");
+
+    await transporter.sendMail({
+      from: process.env.EMAIL,
+      to,
+      subject,
+      text,
+    });
+
+    console.log("Email sent successfully");
+  } catch (error) {
+    console.log("MAIL ERROR:", error);
+  }
+};
 
 export default sendMail;
