f2cb0926014a250f7970f7bc4f815895ecef5a5f
ranjay17 <sranjay15@gmail.com>
2026-06-26 14:03:53 +0530

    fix: sendMail

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -4,35 +4,25 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASS,
   },
-  tls: {
-    rejectUnauthorized: false,
-  },
 });
 
-const sendMail = async (to, subject, text) => {
-  try {
-    console.log("📧 Sending mail...");
-    console.log("EMAIL:", process.env.EMAIL);
-    console.log("PASS EXISTS:", !!process.env.EMAIL_PASS);
-
-    const info = await transporter.sendMail({
-      from: `"Book My Service" <${process.env.EMAIL}>`,
-      to,
-      subject,
-      text,
-    });
-
-    console.log("✅ Email sent successfully:", info.messageId);
-  } catch (error) {
-    console.error("❌ Mail Error:", error.message);
-  }
-};
+const sendMail = async(to, subject, text) =>{
+    try {
+        await transporter.sendMail({
+          from: process.env.EMAIL,
+          to,
+          subject,
+          text,
+        });
+        console.log("Email sent successfully");
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 export default sendMail;
