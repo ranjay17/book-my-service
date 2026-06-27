8337fc1893ff814c92dbe688bf4d9787645ae1ac
ranjay17 <sranjay15@gmail.com>
2026-06-27 12:49:48 +0530

    Fix API base URL for deployment

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ db()
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://bookmyserv.netlify.app","http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
